a5e9bbed48871f283ea8aa63fd072a81aed720f9
Aruzhan Ismagulova <aruzhanism@mail.ru>
2023-06-12 15:29:20 +0600

    TEST

--- cmd/main.go
+++ cmd/main.go
@@ -1,49 +1,66 @@
 package main
 
 import (
-	bpb "github.com/hon/example/pkg/buses/pb"
-	"github.com/hon/example/pkg/config"
-	mpb "github.com/hon/example/pkg/metrics/pb"
-	"github.com/hon/example/pkg/routes"
-	"google.golang.org/grpc"
+	"context"
 	"log"
-	"net"
+	"net/http"
+
+	"github.com/hon/example/gateway/handler"
+	buspb "github.com/hon/example/proto/buses/pb"
+	metricspb "github.com/hon/example/proto/metrics/pb"
+
+	"google.golang.org/grpc"
 )
 
+const (
+	port = ":8000"
+)
+
+type Server struct {
+	metricsClient metricspb.MetricsServiceClient
+	busClient     buspb.BusServiceClient
+}
+
+func (s *Server) RequestBus(ctx context.Context, req *buspb.BusRequest) (*buspb.BusResponse, error) {
+	return s.busClient.RequestBus(ctx, req)
+}
+
+func (s *Server) RequestTemp(ctx context.Context, req *metricspb.TemperatureRequest) (*metricspb.TemperatureResponse, error) {
+	return s.metricsClient.RequestTemp(ctx, req)
+}
+
+func (s *Server) RequestPol(ctx context.Context, req *metricspb.PollutionRequest) (*metricspb.PollutionResponse, error) {
+	return s.metricsClient.RequestPol(ctx, req)
+}
+
 func main() {
-	cfg := config.GetConfig()
-
-	busConn, err := grpc.Dial(cfg.BusesSvcURL, grpc.WithInsecure())
+	metricsConn, err := grpc.Dial("metrics-service:50052", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect to bus service: %v", err)
-	}
-	defer busConn.Close()
-
-	metricsConn, err := grpc.Dial(cfg.MetricsSvcURL, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect to metrics service: %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer metricsConn.Close()
 
-	busClient := bpb.NewBusServiceClient(busConn)
-	metricsClient := mpb.NewMetricsServiceClient(metricsConn)
-
-	gateway := &routes.GatewayServer{
-		BusClient:     busClient,
-		MetricsClient: metricsClient,
-	}
-
-	grpcServer := grpc.NewServer()
-
-	bpb.RegisterBusServiceServer(grpcServer, gateway)
-	mpb.RegisterMetricsServiceServer(grpcServer, gateway)
-
-	lis, err := net.Listen("tcp", cfg.Port)
+	busConn, err := grpc.Dial("bus-service:50053", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer busConn.Close()
+
+	metricsClient := metricspb.NewMetricsServiceClient(metricsConn)
+	busClient := buspb.NewBusServiceClient(busConn)
+
+	s := &Server{
+		metricsClient: metricsClient,
+		busClient:     busClient,
 	}
 
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	http.HandleFunc("/metrics", handler.MetricHandler(s))
+	http.HandleFunc("/bus", handler.HandleBus(s))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			http.ServeFile(w, r, "templates/main.html")
+		}
+	})
+
+	http.ListenAndServe(port, nil)
 }
